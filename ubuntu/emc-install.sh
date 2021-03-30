2b7bff9d815bec1fab451e083198c68fee98a483
PrivateNess.Network <support@privateness.net>
2021-03-30 10:59:29 -0400

    Update emc-install.sh

--- ubuntu/emc-install.sh
+++ ubuntu/emc-install.sh
@@ -6,11 +6,10 @@ getent passwd emc >/dev/null && { echo "User 'emc' already exists"; exit 1; }
 
 apt-get -y install wget pwgen openssl
 
-wget https://dl.dropboxusercontent.com/u/15852900/emercoin/emercoin-0.3.7-linux.tar.gz
-tar xvzf emercoin-0.3.7-linux.tar.gz
-rm emercoin-0.3.7-linux.tar.gz
-cp emercoin-0.3.7-linux/bin/64/emercoind /usr/local/bin
-rm -rf emercoin-0.3.7-linux
+wget https://github.com/NESS-Network/emercoin/archive/refs/tags/v0.7.10emc.tar.gz
+tar xvzf v0.7.10emc.tar.gz
+rm v0.7.10emc.tar.gz
+cp emercoin-0.7.10emc/bin/emc /usr/local/bin
 
 mkdir /tmp/emcskel
 groupadd --gid 500 emc
@@ -18,7 +17,7 @@ useradd -m -d /var/lib/emc -k /tmp/emcskel -s /bin/false --uid 500 --gid 500 emc
 rmdir /tmp/emcskel
 
 mkdir -p /var/lib/emc/ssl
-openssl req -nodes -x509 -newkey rsa:4096 -keyout /var/lib/emc/ssl/emercoin.key -out /var/lib/emc/ssl/emercoin.crt -days 3560 -subj /C=US/ST=Oregon/L=Portland/O=IT/CN=emercoin.local
+openssl req -nodes -x509 -newkey rsa:4096 -keyout /var/lib/emc/ssl/emercoin.key -out /var/lib/emc/ssl/emercoin.crt -days 3560 -subj /C=CA/QC=/L=QC/O=IT/CN=PrivateNess.Network
 
 mkdir -p /var/lib/emc/.emercoin
 cat<<EOF >/var/lib/emc/.emercoin/emercoin.conf
@@ -31,10 +30,10 @@ rpcport=6662
 maxconnections=80
 gen=0
 daemon=1
-rpcssl=1
-rpcsslcertificatechainfile=/var/lib/emc/ssl/emercoin.crt
-rpcsslprivatekeyfile=/var/lib/emc/ssl/emercoin.key
-rpcsslciphers=HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4:!SSLv2
+rpcssl=0
+# rpcsslcertificatechainfile=/var/lib/emc/ssl/emercoin.crt
+# rpcsslprivatekeyfile=/var/lib/emc/ssl/emercoin.key
+# rpcsslciphers=HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4:!SSLv2
 EOF
 
 chmod 600 /var/lib/emc/.emercoin/emercoin.conf
@@ -50,8 +49,4 @@ fi
 sudo -u emc emercoind -datadir=/var/lib/emc/.emercoin \$*
 EOF
 chmod +x /usr/local/bin/emc
-
-wget https://dl.dropboxusercontent.com/u/15852900/emercoin/emercoind.initd
-mv emercoind.initd /etc/init.d/emercoind
-chmod +x /etc/init.d/emercoind
-update-rc.d emercoind defaults
+systemctl enable emercoind
